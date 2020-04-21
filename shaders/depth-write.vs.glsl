be49678ac16d1c2c72970a0d44c96c896db6bb31
Dalia <43527456+dollybuzz@users.noreply.github.com>
2020-04-20 18:48:08 -1000

    done todo 4

--- shaders/depth-write.vs.glsl
+++ shaders/depth-write.vs.glsl
@@ -10,6 +10,5 @@
     void main(void) {
         gl_Position = uProjectionMatrix * uViewMatrix * uWorldMatrix * vec4(aVertexPosition, 1.0);
         // todo convert clip space depth into NDC and rescale from [-1, 1] to [0, 1]
-        
-        vDepth = gl_Position.z;
+        vDepth = (gl_Position.z+1.0)/2.0;
     }
