d3260fc72cd959bf070ece9af02c9e1bd6c8810f
Dalia <43527456+dollybuzz@users.noreply.github.com>
2020-04-20 20:29:34 -1000

    todo 6 done

--- shaders/phong.fs.glsl
+++ shaders/phong.fs.glsl
@@ -19,25 +19,26 @@ void main(void) {
     float specularIntensity = pow(max(dot(reflection01, directionToEye01), 0.0), 64.0);
 
     vec4 texColor = texture2D(uAlbedoTexture, vTexCoords);
-    vec4 shadowColor = texture2D(uShadowTexture, vTexCoords);
+    //vec4 shadowColor = texture2D(uShadowTexture, vTexCoords);
 
-    vec3 ambient = vec3(0.2, 0.2, 0.2) * shadowColor.rgb;
-    vec3 diffuseColor = shadowColor.rgb * lambert;
+    vec3 ambient = vec3(0.2, 0.2, 0.2) * texColor.rgb;
+    vec3 diffuseColor = texColor.rgb * lambert;
     vec3 specularColor = vec3(1.0, 1.0, 1.0) * specularIntensity;
     vec3 finalColor = ambient + diffuseColor + specularColor;
 
     // todo #6
     // transform the world position into the lights clip space
-    //vec4 lightSpaceNDC = ?
+    vec4 lightSpaceNDC = uLightVPMatrix * vec4(vWorldPosition, 1.0);
 
     // transform the clip space position into NDC (will already be in NDC for orthographic projection but we do it just in case)
-    //lightSpaceNDC = ?
+    float xCoord = (lightSpaceNDC.x + 1.0)/2.0;
+    float yCoord = (lightSpaceNDC.y + 1.0)/2.0;
 
     // scale and bias the light-space NDC xy coordinates from [-1, 1] to [0, 1]
-    //vec2 lightSpaceUV = ?
+    vec2 lightSpaceUV = vec2(xCoord, yCoord);
 
     // todo #8 scale and bias the light-space NDC z coordinate from [-1, 1] to [0, 1]
-    //float lightDepth = ?
+    //float lightDepth = (lightSpaceNDC+1.0)/2.0;
 
     // use this as part of todo #10
     float bias = 0.004;
@@ -46,8 +47,11 @@ void main(void) {
     // Sample from the shadow map texture using the previously calculated lightSpaceUV
     // vec4 shadowColor = ?
 
-    // todo #9
-    gl_FragColor = vec4(finalColor, 1.0); // remove this when you are ready to add shadows
+    // todo #6
+    gl_FragColor = vec4(lightSpaceUV.x, lightSpaceUV.y, 0.0, 1.0);
+
+    // todo #9 
+    // remove this when you are ready to add shadows
     //if (/* in shadow*/) {
     //   gl_FragColor = vec4(ambient, 1.0);
     //} else {
